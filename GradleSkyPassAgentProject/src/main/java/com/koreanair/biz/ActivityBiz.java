30af8604a28e1cc1c78f45c2f900a5bb55b13fe7
sojaeyul <sojaeyul@DESKTOP-21M3D07>
2022-12-21 17:33:01 +0900

    구조변경

--- GradleSkyPassAgentProject/src/main/java/com/koreanair/biz/ActivityBiz.java
+++ GradleSkyPassAgentProject/src/main/java/com/koreanair/biz/ActivityBiz.java
@@ -5,14 +5,14 @@ import java.util.HashMap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class MembershipBiz {
-	private final static Logger log = LoggerFactory.getLogger(MembershipBiz.class);
-	public MembershipBiz() {
-		System.out.println("MembershipBiz 생성자 호출");
+public class ActivityBiz {
+	private final static Logger log = LoggerFactory.getLogger(ActivityBiz.class);
+	public ActivityBiz() {
+		System.out.println("ActivityBiz 생성자 호출");
 	}
 
 	public String doJsonToDBCreate(HashMap<String, Object> jsonMap) {
-		System.out.println("MembershipBiz.doJsonForDBCreate Call!!!!!!!!!!!!!!!!!!!!!!!!" + jsonMap.toString());
+		System.out.println("ActivityBiz.doJsonForDBCreate Call!!!!!!!!!!!!!!!!!!!!!!!!" + jsonMap.toString());
 		
 		//1. parsing start
 		/*
